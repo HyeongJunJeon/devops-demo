6373abea6c71055a37ce48c7b5c17a1ee86a6dc0
HyeongJunJeon <wehj0105@gmail.com>
2023-01-30 10:07:19 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">bloco FE-Devops-demo 입니다. by 전형준</div>;
+  return <div className="App">bloco FE-Devops-demo 입니다. build test</div>;
 }
 
 export default App;
