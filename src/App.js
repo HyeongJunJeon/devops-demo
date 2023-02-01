31da5a5562af6894c8fa30a3eb025d56b65f6727
HyeongJunJeon <wehj0105@gmail.com>
2023-02-01 13:44:20 +0900

    lint test9

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">bloco FE-Devops-demo입니다. build test</div>;
+  return <div className="App"bloco FE-Devops-demo입니다. build test</div>;
 }
 
 export default App;
